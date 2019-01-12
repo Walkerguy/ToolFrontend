3fe1ed10df34097025810ef461e04254b7bd6e9c
Ruben <rcbruijn1@avans.nl>
2019-01-12 14:53:16 +0100

    basic layout dashboard / navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ export class AppComponent {
   title = 'voorbeeld';
 
   @ViewChild('mySideNav') public mySideNav: ElementRef;
+  @ViewChild('menuButton') public menuButton: ElementRef;
 
 
   openNav(){
     this.mySideNav.nativeElement.style.width = '250px'
+    this.menuButton.nativeElement.style.visibility = 'hidden'
   }
 
   closeNav(){
     this.mySideNav.nativeElement.style.width = '0px'
+    this.menuButton.nativeElement.style.visibility = 'visible'
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes : Routes = [
+  { path: '', component: DashboardComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
