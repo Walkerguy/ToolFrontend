cd54475a6748f42d36df73748e04417b70357b2d
Yoki <yboeckholtz@gmail.com>
2019-01-15 17:17:29 +0100

    Rentpage almost finished, multiplier to be fixed. All data gets loaded into the modal

--- test/ToolFrontend/src/app/models/product.ts
+++ test/ToolFrontend/src/app/models/product.ts
@@ -4,12 +4,14 @@ export class Product {
     public category: string;
     public description: string;
     public imageUrl: string;
+    public price: number;
 
-    constructor(name: string, category: string, description: string, imageUrl: string)
+    constructor(name: string, category: string, description: string, imageUrl: string, price: number)
     {
         this.name = name;
         this.category = category;
         this.description = description;
         this.imageUrl = imageUrl;
+        this.price = price;
     }
 }
--- test/ToolFrontend/src/app/seperateComponents/tool-card/tool-card.component.ts
+++ test/ToolFrontend/src/app/seperateComponents/tool-card/tool-card.component.ts
@@ -7,14 +7,21 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./tool-card.component.css']
 })
 export class ToolCardComponent implements OnInit {
+  public activeProduct: any;
+
+ 
   products: Product[] = [
-    new Product('Hammer', 'Hammer', 'Item used to hammer things', 'https://upload.wikimedia.org/wikipedia/commons/8/84/Claw-hammer.jpg'),
-    new Product('Saw', 'Hammer', 'I just saw a guy saying the n-word', 'https://media.screwfix.com/is/image//ae235?src=ae235/32045_P&$prodImageMedium$') 
+    new Product('Hammer', 'Hammer', 'Item used to hammer things', 'https://upload.wikimedia.org/wikipedia/commons/8/84/Claw-hammer.jpg', 1),
+    new Product('Saw', 'Hammer', 'I just saw a guy saying the n-word', 'https://media.screwfix.com/is/image//ae235?src=ae235/32045_P&$prodImageMedium$', 2),
+    new Product('Hit or miss', 'Hit or miss', 'I guess they never miss huh, mwah', 'https://pbs.twimg.com/media/Ds5mk0RU0AA1z_l.jpg', 5)
     
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  public openModal(product): void{
+    // Copying object reference so we dont modify the original
+    this.activeProduct = Object.assign({}, product);
+}
 }
